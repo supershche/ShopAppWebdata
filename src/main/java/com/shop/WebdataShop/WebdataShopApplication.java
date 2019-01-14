d9a27951c64da3e42bf8c61762b83d21c234ae71
AnastasiiaK <anastasiia.kozachuk@scalors.com>
2019-01-14 08:47:57 +0200

    Initialize SpringBoot App

---
+++ src/main/java/com/shop/WebdataShop/WebdataShopApplication.java
@@ -0,0 +1,14 @@
+package com.shop.WebdataShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdataShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdataShopApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/shop/WebdataShop/WebdataShopApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shop.WebdataShop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebdataShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
